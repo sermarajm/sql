7699079a2781522f8d5a4382260aa629be362d93
sermarajm <147028235+sermarajm@users.noreply.github.com>
2025-08-14 19:34:02 +0530

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,11 @@
+use employeesdetail;
+
+update employeesdetail
+set department = 'rii'
+where firstname = 'serma';
+
+select* from 	 			 employeesdetail;
+select * from employeesdetail where department = 'eee';
+
+
+
