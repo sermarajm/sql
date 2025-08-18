799e549441b56f09ed1878ba02b397275f34821e
sermarajm <147028235+sermarajm@users.noreply.github.com>
2025-08-18 08:24:09 +0530

    Add files via upload

---
+++ sql4.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE companyDB;
+
+USE companyDB;
+
+CREATE TABLE sermar (
+    s_no INT,
+    name CHAR,
+    age INT,
+    num BIGINT
+);
+
+select * from sermar;
+-- drop table  serma;
+
+
+
