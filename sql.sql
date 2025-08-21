28490d2a44979cb951be7d32d012e736b422fefa
sermarajm <147028235+sermarajm@users.noreply.github.com>
2025-08-21 10:58:37 +0530

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -1,11 +1,33 @@
-use employeesdetail;
+CREATE DATABASE group_by;
+USE group_by;
 
-update employeesdetail
-set department = 'rii'
-where firstname = 'serma';
+CREATE TABLE employee(
+e_id INT PRIMARY KEY,
+e_name VARCHAR(50) NOT NULL,
+job_desc VARCHAR(50),
+salary int);
 
-select* from 	 			 employeesdetail;
-select * from employeesdetail where department = 'eee';
+INSERT INTO employee
+VALUES
+(1,'Joseph','Developer',25000),
+(2,'Revathy','HR',30000),
+(3,'Nisha','Developer',20000),
+(4,'Pramoth','HR',35000),
+(5,'Malik','Tester',10000),
+(6,'Suresh','Tester',15000),
+(7,'Shanthi','Admin',8000),
+(8,'Sundar','Admin',10000),
+(9,'Pramela','Manager',80000),
+(10,'Kesavan','CEO',100000),
+(11,'Kedharnath','Manager',75000),
+(12,'Ameer','Developer',30000),
+(13,'Ashok','Tester',30000),
+(14,'Sam','Analyst',25000),
+(15,'Joshibha','Analyst',35000),
+(16,'Hari','Analyst',45000);
 
+select * from employee;
 
+select job_desc,sum(salary) from employee group by job_desc;
 
+select job_desc,sum(salary) from employee group by job_desc order by sum(salary) desc;
